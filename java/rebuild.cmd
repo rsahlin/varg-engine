44ec48ae96b627a7c7b371129b2414cf1a84388a
Richard Sahlin <richard_sahlin@yahoo.com>
2024-05-28 17:07:04 +0200

    Add executable jar for linux aarch64

--- java/rebuild.cmd
+++
@@ -1,3 +0,0 @@
-call mvn -f C:\source\gltf-io\java\pom.xml clean compile install -DskipTests
-call mvn -f C:\source\gltf-imageio\java\pom.xml clean compile install -DskipTests
-call mvn clean compile install -DskipTests
