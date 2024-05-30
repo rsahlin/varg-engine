ca6a79ddd0b978a2b911c76e949402c693963eca
Richard Sahlin <richard_sahlin@yahoo.com>
2024-05-30 11:25:33 +0200

    Update LWJGL3VARGTest.java

--- java/varg-lwjgl3/src/test/java/org/varg/lwjgl3/test/LWJGL3VARGTest.java
+++ java/varg-lwjgl3/src/test/java/org/varg/lwjgl3/test/LWJGL3VARGTest.java
@@ -3,13 +3,16 @@ package org.varg.lwjgl3.test;
 
 import java.nio.ByteBuffer;
 
-import org.gltfio.deserialize.LaddaProperties;
+import org.gltfio.deserialize.Ladda.LaddaBooleanProperties;
+import org.gltfio.deserialize.Ladda.LaddaFloatProperties;
+import org.gltfio.deserialize.Ladda.LaddaProperties;
 import org.gltfio.lib.BitFlags;
 import org.gltfio.lib.Buffers;
 import org.gltfio.lib.FileUtils.FilesystemProperties;
 import org.gltfio.lib.Settings;
 import org.varg.BackendException;
 import org.varg.lwjgl3.apps.VARGViewer;
+import org.varg.renderer.BRDF.BRDFFloatProperties;
 import org.varg.renderer.Renderers;
 import org.varg.shader.PickingRayTracingShader.PickingRayTracingShaderCreateInfo;
 import org.varg.shader.RayTracingShader;
@@ -56,67 +59,41 @@ public class LWJGL3VARGTest extends VARGViewer {
 
     @Override
     protected String getDefaultModelName() {
-        // return "STRANDMON.glb";
-        // return "20456956.glb"; // strandmon chequered
-        // return "30515314.glb"; // listerby
-        // return "10277366.glb"; //v�rdera kopp
-        // return "00436714.glb"; // kivik soffa
-        // return "shaf/000033e5-b119-476d-8d56-f7a46b7f82bb.glb";
-        // return "shaf/0000d5d9-b08a-4472-8bf8-2126c8e06edb.glb";
-        // return "shaf/00043b80-10cb-40a8-acfd-ddab7ec80e3c.glb";
-        // return "shaf/0008a0d2-8399-414d-8b5d-1e401628bc01.glb";
-        // return "shaf/000427bf-b206-42bc-93f4-ff9ca09e1496.glb";
-        // return "TestSpheresNoTextures.gltf";
-        // return "gltf/BINGSTA/BINGSTA.glb";
-        // return "DisplayEncodingPQ-yellowlight.gltf";
-        // return "MaterialSample_v02.gltf";
-        // return "WaterBottle.gltf";
-        // return "AnimatedCube/glTF/AnimatedCube.gltf";
-        // return "WaterBottle/glTF/WaterBottle.gltf";
+        // return "gltf/kudde/kudde.glb";
+        // return "WaterBottle/glTF-Binary/WaterBottle.glb";
         // return "Corset/glTF-Binary/Corset.glb";
         // return "FlightHelmet/glTF/FlightHelmet.gltf";
-        // return "Duck/glTF/Duck.gltf";
-        // return "Avocado/glTF/Avocado.gltf";
+        return "PlaysetLightTest/glTF-Binary/PlaysetLightTest.glb";
+        // return "Duck/glTF-Binary/Duck.glb";
+        // return "Avocado/glTF-Binary/Avocado.glb";
         // return "TextureTransformTest/glTF/TextureTransformTest.gltf";
         // return "2CylinderEngine/glTF/2CylinderEngine.gltf";
+        // return "ClearcoatWicker/glTF-Binary/ClearcoatWicker.glb";
         // return "GearboxAssy/glTF/GearboxAssy.gltf";
         // return "OrientationTest/glTF/OrientationTest.gltf";
+        // return "MetalRoughSpheresNoTextures/glTF-Binary/MetalRoughSpheresNoTextures.glb";
         // return "BoxVertexColors/glTF/BoxVertexColors.gltf";
         // return "BoxTextured/glTF/BoxTextured.gltf";
-        // return "LightsEnvironment.gltf";
-        // return "TextureCoordinateTest.gltf";
-        // return "NormalTangentTest.glb";
-        // return "MetalRoughSpheresNoTextures.glb";
-        // return "NormalTextureWithTangents.gltf";
-        // return "roughnesstest.glb";
-        // return "iortest_underwater.gltf";
-        // return "10000_astroid_cubes.glb";
-        // return "10000asteroids/asteroids_textures_v02.glb";
-        // return "NodePerformanceTest/glTF-Binary/NodePerformanceTest.glb";
         // return "ABeautifulGame/glTF/AbeautifulGame.gltf";
-        // return "TwoSidedPlane/glTF/TwoSidedPlane.gltf";
         // return "Sponza/glTF/Sponza.gltf";
-        // return "gltf/car_toy_cartoon_style/car_toy_cartoon_style.glb";
-        // return "extensions.gltf";
-        // return "EKET_00333947_70x35x35.glb";
-        return "shaf/input/9f530490-31b3-4a18-98bc-55b375ce19a3.glb";
         // return "CREATOR:save2.gltf";
-        // return "gltfio-creator/indoor_2.glb";
+        // return "gltf/MacbethBalls/MacbethBalls.glb";
+        // return "gltf/clearcoat_2.glb";
     }
 
     @Override
     protected String getResourcePath() {
         // return "C:/assets/test-assets/";
-        return "C:/";
-        // return "C:/source/glTF-Sample-Assets/Models/";
+        return "C:/source/glTF-Sample-Assets/Models/";
         // return "C:/source/glTF-Sample-Models/2.0/";
-        // return "C:/assets/test-assets/gltfio-creator/";
-        // return "C:/source/IKEA3DAssemblyDataset/Dataset/EKET_00333947_70x35x35/glTF_glb";
     }
 
     @Override
     protected void init() {
         super.init();
+        // sceneControl.getCurrentScene().getSceneTransform().setTranslate(new float[] { 0.29f, -0.0017777674f, -0.09600001f });
+        // sceneControl.rotateScene(new float[] { -0.0060974793f, 0.99903536f, 0.0030282282f, 0.043017954f });
+        // sceneControl.rotateScene(new float[] { 0f, -0.0871557f, 0f, 0.9961947f });
         try {
             boolean createTracer = false;
             if (createTracer) {
@@ -131,22 +108,16 @@ public class LWJGL3VARGTest extends VARGViewer {
             org.varg.vulkan.extensions.KHRAccelerationStructure<Queue> accelerationExtension) {
         int geometryUsageFlags = BitFlags.getFlagsValue(BufferUsageFlagBit.VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
                 org.varg.vulkan.extensions.KHRAccelerationStructure.BufferUsageFlagBit.VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR);
-        MemoryBuffer geometryBuffer =
-                allocator.createBuffer(aabbCount * AccelerationStructureGeometryAabbsDataKHR.AABB_SIZE_IN_BYTES,
-                        geometryUsageFlags);
+        MemoryBuffer geometryBuffer = allocator.createBuffer(aabbCount * AccelerationStructureGeometryAabbsDataKHR.AABB_SIZE_IN_BYTES, geometryUsageFlags);
         Memory geometryMemory = allocator.allocateMemory(geometryBuffer.allocationSize,
                 BitFlags.getFlagsValue(MemoryPropertyFlagBit.VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT),
                 BitFlags.getFlagsValue(Vulkan12.MemoryAllocateFlagBits.VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_BIT), 0);
         allocator.bindBufferMemory(geometryBuffer, geometryMemory, 0);
         DeviceOrHostAddress geometryAddress = accelerationExtension.getBufferDeviceAddress(geometryBuffer);
-        AccelerationStructureGeometryAabbsDataKHR geometries =
-                new AccelerationStructureGeometryAabbsDataKHR(geometryAddress);
-        AccelerationStructureGeometryKHR geometry =
-                new AccelerationStructureGeometryKHR(GeometryTypeKHR.VK_GEOMETRY_TYPE_AABBS_KHR, geometries);
+        AccelerationStructureGeometryAabbsDataKHR geometries = new AccelerationStructureGeometryAabbsDataKHR(geometryAddress);
+        AccelerationStructureGeometryKHR geometry = new AccelerationStructureGeometryKHR(GeometryTypeKHR.VK_GEOMETRY_TYPE_AABBS_KHR, geometries);
 
-        AccelerationStructureBuildGeometryInfoKHR geometryInfo = new AccelerationStructureBuildGeometryInfoKHR(
-                AccelerationStructureTypeKHR.VK_ACCELERATION_STRUCTURE_TYPE_BOTTOM_LEVEL_KHR,
-                geometry, aabbCount,
+        AccelerationStructureBuildGeometryInfoKHR geometryInfo = new AccelerationStructureBuildGeometryInfoKHR(AccelerationStructureTypeKHR.VK_ACCELERATION_STRUCTURE_TYPE_BOTTOM_LEVEL_KHR, geometry, aabbCount,
                 BuildAccelerationStructureFlagBitsKHR.VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR);
 
         return geometryInfo;
@@ -156,22 +127,16 @@ public class LWJGL3VARGTest extends VARGViewer {
             org.varg.vulkan.extensions.KHRAccelerationStructure<Queue> accelerationExtension) {
         int geometryUsageFlags = BitFlags.getFlagsValue(BufferUsageFlagBit.VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
                 org.varg.vulkan.extensions.KHRAccelerationStructure.BufferUsageFlagBit.VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR);
-        MemoryBuffer geometryBuffer =
-                allocator.createBuffer(AccelerationStructureGeometryInstancesDataKHR.INSTANCE_SIZE_IN_BYTES,
-                        geometryUsageFlags);
-        Memory geometryMemory = allocator.allocateMemory(geometryBuffer.allocationSize,
-                BitFlags.getFlagsValue(MemoryPropertyFlagBit.VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT),
+        MemoryBuffer geometryBuffer = allocator.createBuffer(AccelerationStructureGeometryInstancesDataKHR.INSTANCE_SIZE_IN_BYTES,
+                geometryUsageFlags);
+        Memory geometryMemory = allocator.allocateMemory(geometryBuffer.allocationSize, BitFlags.getFlagsValue(MemoryPropertyFlagBit.VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT),
                 BitFlags.getFlagsValue(Vulkan12.MemoryAllocateFlagBits.VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_BIT), 0);
         allocator.bindBufferMemory(geometryBuffer, geometryMemory, 0);
         DeviceOrHostAddress geometryAddress = accelerationExtension.getBufferDeviceAddress(geometryBuffer);
-        AccelerationStructureGeometryInstancesDataKHR instances =
-                new AccelerationStructureGeometryInstancesDataKHR(geometryAddress);
-        AccelerationStructureGeometryKHR geometry =
-                new AccelerationStructureGeometryKHR(GeometryTypeKHR.VK_GEOMETRY_TYPE_INSTANCES_KHR, instances);
+        AccelerationStructureGeometryInstancesDataKHR instances = new AccelerationStructureGeometryInstancesDataKHR(geometryAddress);
+        AccelerationStructureGeometryKHR geometry = new AccelerationStructureGeometryKHR(GeometryTypeKHR.VK_GEOMETRY_TYPE_INSTANCES_KHR, instances);
 
-        AccelerationStructureBuildGeometryInfoKHR geometryInfo = new AccelerationStructureBuildGeometryInfoKHR(
-                AccelerationStructureTypeKHR.VK_ACCELERATION_STRUCTURE_TYPE_TOP_LEVEL_KHR,
-                geometry, 1,
+        AccelerationStructureBuildGeometryInfoKHR geometryInfo = new AccelerationStructureBuildGeometryInfoKHR(AccelerationStructureTypeKHR.VK_ACCELERATION_STRUCTURE_TYPE_TOP_LEVEL_KHR, geometry, 1,
                 BuildAccelerationStructureFlagBitsKHR.VK_BUILD_ACCELERATION_STRUCTURE_PREFER_FAST_TRACE_BIT_KHR);
 
         return geometryInfo;
@@ -182,32 +147,24 @@ public class LWJGL3VARGTest extends VARGViewer {
             AccelerationStructureBuildSizesInfoKHR buildSize,
             org.varg.vulkan.extensions.KHRAccelerationStructure<Queue> accelerationExtension) {
 
-        MemoryBuffer accelerationBuffer = allocator.createBuffer(buildSize.accelerationStructureSize,
-                org.lwjgl.vulkan.KHRAccelerationStructure.VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_STORAGE_BIT_KHR);
-        Memory accelerationMemory = allocator.allocateMemory(accelerationBuffer.allocationSize,
-                BitFlags.getFlagsValue(MemoryPropertyFlagBit.VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT));
+        MemoryBuffer accelerationBuffer = allocator.createBuffer(buildSize.accelerationStructureSize, org.lwjgl.vulkan.KHRAccelerationStructure.VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_STORAGE_BIT_KHR);
+        Memory accelerationMemory = allocator.allocateMemory(accelerationBuffer.allocationSize, BitFlags.getFlagsValue(MemoryPropertyFlagBit.VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT));
         allocator.bindBufferMemory(accelerationBuffer, accelerationMemory, 0);
         return accelerationBuffer;
     }
 
-    private AccelerationStructureKHR createAccelerationStructure(DeviceMemory allocator,
-            AccelerationStructureBuildSizesInfoKHR buildSize, MemoryBuffer accelerationBuffer,
+    private AccelerationStructureKHR createAccelerationStructure(DeviceMemory allocator, AccelerationStructureBuildSizesInfoKHR buildSize, MemoryBuffer accelerationBuffer,
             org.varg.vulkan.extensions.KHRAccelerationStructure<Queue> accelerationExtension) {
-        AccelerationStructureCreateInfoKHR createInfo = new AccelerationStructureCreateInfoKHR(accelerationBuffer,
-                buildSize.accelerationStructureSize,
-                buildSize.getBuildInfo().type);
+        AccelerationStructureCreateInfoKHR createInfo = new AccelerationStructureCreateInfoKHR(accelerationBuffer, buildSize.accelerationStructureSize, buildSize.getBuildInfo().type);
 
         AccelerationStructureKHR acceleration = accelerationExtension.createAccelerationStructureKHR(createInfo);
         return acceleration;
     }
 
     private MemoryBuffer createScratchBuffer(DeviceMemory allocator, AccelerationStructureBuildSizesInfoKHR buildSize) {
-        int usageFlags = BitFlags.getFlagsValue(BufferUsageFlagBit.VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
-                BufferUsageFlagBit.VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT);
-        MemoryBuffer scratchBuffer =
-                allocator.createBuffer(buildSize.buildScratchSize, usageFlags);
-        Memory scratchMemory = allocator.allocateMemory(scratchBuffer.allocationSize,
-                BitFlags.getFlagsValue(MemoryPropertyFlagBit.VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT),
+        int usageFlags = BitFlags.getFlagsValue(BufferUsageFlagBit.VK_BUFFER_USAGE_STORAGE_BUFFER_BIT, BufferUsageFlagBit.VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT);
+        MemoryBuffer scratchBuffer = allocator.createBuffer(buildSize.buildScratchSize, usageFlags);
+        Memory scratchMemory = allocator.allocateMemory(scratchBuffer.allocationSize, BitFlags.getFlagsValue(MemoryPropertyFlagBit.VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT),
                 BitFlags.getFlagsValue(Vulkan12.MemoryAllocateFlagBits.VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_BIT), 0);
         allocator.bindBufferMemory(scratchBuffer, scratchMemory, 0);
         return scratchBuffer;
@@ -235,10 +192,8 @@ public class LWJGL3VARGTest extends VARGViewer {
 
         AccelerationStructureBuildRangeInfoKHR rangeInfo = new AccelerationStructureBuildRangeInfoKHR(aabbCount, 0);
 
-        AccelerationStructureBuildGeometryInfoKHR[] infos =
-                new AccelerationStructureBuildGeometryInfoKHR[] { geometryInfo };
-        AccelerationStructureBuildRangeInfoKHR[] buildRangeInfos =
-                new AccelerationStructureBuildRangeInfoKHR[] { rangeInfo };
+        AccelerationStructureBuildGeometryInfoKHR[] infos = new AccelerationStructureBuildGeometryInfoKHR[] { geometryInfo };
+        AccelerationStructureBuildRangeInfoKHR[] buildRangeInfos = new AccelerationStructureBuildRangeInfoKHR[] { rangeInfo };
         Queue queue = getRenderer().getQueue();
         accelerationExtension.cmdBuildAccelerationStructuresKHR(queue, infos, buildRangeInfos);
 
@@ -288,6 +243,7 @@ public class LWJGL3VARGTest extends VARGViewer {
 
     public static void main(String[] args) {
         Settings settings = Settings.getInstance();
+        settings.setProperty(LaddaBooleanProperties.SELECT_RUNTIME_CAMERA, false);
         settings.setProperty(FilesystemProperties.JAVA_TARGET_DIRECTORY, "target/test-classes");
         settings.setProperty(FilesystemProperties.SOURCE_DIRECTORY, "src/test");
         settings.setProperty(BackendIntProperties.SURFACE_WIDTH, 1920);
@@ -297,13 +253,17 @@ public class LWJGL3VARGTest extends VARGViewer {
         settings.setProperty(BackendIntProperties.FRAGMENTSIZE, 1);
         settings.setProperty(BackendIntProperties.SAMPLE_COUNT, 8);
         settings.setProperty(BackendStringProperties.COLORSPACE, "VK_COLOR_SPACE_SRGB_NONLINEAR_KHR");
-        settings.setProperty(BackendIntProperties.MAX_WHITE, 500);
-        settings.setProperty(IntArrayProperties.CLEAR_COLOR, 0, 0, 0, 255);
+        settings.setProperty(BackendIntProperties.MAX_WHITE, 1500);
+        settings.setProperty(IntArrayProperties.CLEAR_COLOR, 90, 90, 90, 255);
+        settings.setProperty(LaddaFloatProperties.MATERIAL_ABSORPTION, 0.2f);
+        settings.setProperty(BRDFFloatProperties.NDF_FACTOR, 3.0f);
+        settings.setProperty(BRDFFloatProperties.SOLIDANGLE_FUDGE, 0.001f);
+        settings.setProperty(LaddaFloatProperties.BACKGROUND_INTENSITY_SCALE, 0.4f);
 
-        settings.setProperty(LaddaProperties.IRRADIANCEMAP, "intensity:500|irmap:STUDIO_5");
-        // settings.setProperty(LaddaProperties.ENVMAP_BACKGROUND, "SH");
-        // settings.setProperty(LaddaProperties.DIRECTIONAL_LIGHT, "intensity:17000|color:1,1,1|position:0,10000,1000");
-        // settings.setProperty(LaddaProperties.DIRECTIONAL_LIGHT1,"intensity:5000|color:1,0,0|position:-1000,0,10000");
+        settings.setProperty(LaddaProperties.IRRADIANCEMAP, "intensity:600|irmap:STUDIO_5");
+        settings.setProperty(LaddaProperties.ENVMAP_BACKGROUND, "SH");
+        settings.setProperty(LaddaProperties.DIRECTIONAL_LIGHT, "intensity:1500|color:1,1,1|position:0,10000,10000");
+        // settings.setProperty(LaddaProperties.DIRECTIONAL_LIGHT1, "intensity:5000|color:1,0,0|position:-1000,0,10000");
         // settings.setProperty(LaddaProperties.DIRECTIONAL_LIGHT2, "intensity:5000|color:0,1,0|position:1000,0,10000");
         // settings.setProperty(LaddaProperties.DIRECTIONAL_LIGHT3, "intensity:5000|color:0,0,1|position:0,10000,00");
         // settings.setProperty(LaddaProperties.ENVIRONMENTMAP,
